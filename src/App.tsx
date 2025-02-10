19534ac9f98fdb59aa167c902023e2c0e8fc6e0c
techno161 <kaushal.dhrangad.ts@outlook.com>
2025-02-10 12:10:31 +0530

    Add 404 page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-// import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Sidebar from "./components/sidebar/Sidebar";
-// import Test from "./components/Test";
-// import Test from "./components/Task";
-import Command from "./components/Command";
 import AddTask from "./components/AddTask";
 import Layout from "./components/layout/Layout";
 import NoPage from "./components/NoPage";
-// import Sidebar from "./components/sidebar/Sidebar";
 function App() {
   return (
     <>
@@ -20,12 +14,7 @@ function App() {
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
-      {/* <Layout> */}
-      {/* <Sidebar />
-        <AddTask /> */}
-      {/* <Command /> */}
-      {/* <Task /> */}
-      {/* </Layout> */}
+
     </>
   );
 }
--- src/components/NoPage.tsx
+++ src/components/NoPage.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Link } from "react-router";
+import { Button } from "./ui/button";
 
 const NoPage = () => {
   return (
-    <div>NoPage</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center h-screen ">
+      <div className="text-8xl">404</div>
+      <div className="text-2xl">Uh Oh, That page doesn't exist.</div>
+      <Link to="/">
+      {/* 7acf71 */}
+        <Button className="px-3 py-2 font-bold mt-3 rounded-lg"> 
+          Go to Home
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
-export default NoPage
+export default NoPage;
